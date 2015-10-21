a00474349401e3247c134c41df3573e04d042710
tristangaebler <tgae6529@740s-pl-115.local>
2015-10-21 09:10:02 -0600

    Created the ChatView file

--- src/chat/controller/ChatController.java
+++ src/chat/controller/ChatController.java
@@ -1,11 +1,16 @@
 package chat.controller;
+
+import chat.view.ChatView;
+
 /**
  * Application controller for the Chatbot project.
  * @author Tristan Gaebler
- * @ersion 1.0 10/21/15
+ * @version 1.11 10/21/15 Imported ChatVeiw. Fixed the error.
  */
 public class ChatController 
 {
+	private ChatView myDisplay;
+	
 	public void start()
 	{
 		
---
+++ src/chat/view/ChatView.java
@@ -0,0 +1,34 @@
+package chat.view;
+
+import javax.swing.JOptionPane;
+/**
+ * Popup display to get input and show output.
+ * @author Tristan Gaebler
+ * @version 1.0 10/21/15
+ */
+
+public class ChatView 
+{
+	/**
+	 * Displays the input from the user.
+	 * @param userInput
+	 */
+	public void showOutput(String userInput)
+	{
+		JOptionPane.showMessageDialog(null, userInput);
+	}
+	
+	/**
+	 * Grabs the user input and uses JOptionPane to display it.
+	 * @param displayText
+	 * @return userAnswer
+	 */
+	public String grabInput(String displayText)
+	{
+		String userAnswer = " ";
+		
+		JOptionPane.showInputDialog(null, displayText);
+		
+		return userAnswer;
+	}
+}
