267a6442a36ce374d9879542f1fb69f95ca5743b
tristangaebler <tgae6529@740s-pl-115.local>
2015-10-23 08:20:07 -0600

    Added the string checker in the && form.

--- src/chat/model/Chatbot.java
+++ src/chat/model/Chatbot.java
@@ -42,7 +42,15 @@ public class Chatbot
 	 */
 	public boolean lengthChecker(String currentInput)
 	{
-		return false;
+		boolean hasLength = false;
+		
+		if(currentInput != null && currentInput.length() > 0)
+		{
+			hasLength = true;
+		}
+		
+		return hasLength;
+		
 	}
 	
 	/**
