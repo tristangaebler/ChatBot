2a5323b40b27b0f24096f06b864ccb33f89bd31c
tristangaebler <tgae6529@740s-pl-115.local>
2015-11-05 08:51:18 -0700

    Added an icon to my project. Added documentation.

--- src/chat/view/ChatView.java
+++ src/chat/view/ChatView.java
@@ -1,23 +1,34 @@
 package chat.view;
 
 import javax.swing.JOptionPane;
+import javax.swing.ImageIcon;
 
 
 /**
  * Popup display to get input and show output.
  * @author Tristan Gaebler
- * @version 1.0 10/21/15
+ * @version 1.3 11/5/15 Added icons to my project!
  */
 
 public class ChatView 	 
 {
+	private String windowMessage;
+	private ImageIcon chatIcon;
+	
+	
+	public ChatView ()
+	{
+		windowMessage = "Hello from the computer";
+		chatIcon = new ImageIcon(getClass().getResource("images/Chat.png"));
+	}
+	
 	/**
 	 * Displays the input from the user.
 	 * @param userInput
 	 */
 	public void showOutput(String userInput)
 	{
-		JOptionPane.showMessageDialog(null, userInput);
+		JOptionPane.showMessageDialog(null, userInput, windowMessage, JOptionPane.INFORMATION_MESSAGE, chatIcon);
 	}
 	
 	/**
@@ -27,7 +38,7 @@ public class ChatView
 	 */
 	public String grabInput(String displayText)
 	{
-		String userAnswer = JOptionPane.showInputDialog(null, displayText);
+		String userAnswer = JOptionPane.showInputDialog(null, displayText, windowMessage, JOptionPane.PLAIN_MESSAGE, chatIcon, null, "Answer here:").toString();
 		
 		return userAnswer;
 	}
