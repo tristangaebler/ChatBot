1a77369e9da126d3b15446dfa74c8f12d1562d80
tristangaebler <tgae6529@740s-pl-115.local>
2015-12-04 08:15:15 -0700

    Added documentation to ChatFrame

--- src/chat/view/ChatFrame.java
+++ src/chat/view/ChatFrame.java
@@ -3,7 +3,11 @@ package chat.view;
 import javax.swing.JFrame;
 import java.awt.Color;
 import chat.controller.ChatController;
-
+/**
+ * Setting up the frame and making the ChatBot look nice...trying not making Cody's eyes bleed. 
+ * @author Tristan Gaebler
+ * @version 1.4 Added documentation to ChatFrame
+ */
 public class ChatFrame extends JFrame
 {
 	private ChatController baseController;
@@ -16,7 +20,10 @@ public class ChatFrame extends JFrame
 		basePanel = new ChatPanel(baseController);
 		setUpFrame();
 	}
-	
+	/**
+	 * This is were my frame is set up
+	 * Setting values to show the frame
+	 */
 	private void setUpFrame()
 	{
 		this.setContentPane(basePanel);
@@ -27,6 +34,10 @@ public class ChatFrame extends JFrame
 		this.setVisible(true);
 	}
 	
+	/**
+	 * Getter for baseController
+	 * @return
+	 */
 	public ChatController getBaseController()
 	{
 		return baseController;
