6bbe5481f0bb8039e89bd758668b4d4958225e6d
tristangaebler <Tristan.Gaebler@gmail.com>
2016-03-07 08:56:49 -0700

    Added two arraylists

--- src/chat/model/CTECTwitter.java
+++ src/chat/model/CTECTwitter.java
@@ -1,8 +1,20 @@
 package chat.model;
 
 import java.util.ArrayList;
+import twitter4j.*;
+/**
+ * 
+ * @author Tristan Gaebler
+ * @version 0.1
+ * 
+ * This is the CTECTwitter class that will gather and sort information. It will use the Twitter API to gather data, and java to sort that data. Going to interact with twitter.
+ *
+ */
 
 public class CTECTwitter
 {
+	private ArrayList<Status> statusList;
+	private ArrayList<String> wordList;
+	
 	
 }
