d205f63b1366ad61ac1d278294b08d9e3649ca0c
tristangaebler <Tristan.Gaebler@gmail.com>
2016-03-07 09:14:50 -0700

    Added the sendTWeet method

--- src/chat/model/CTECTwitter.java
+++ src/chat/model/CTECTwitter.java
@@ -1,11 +1,12 @@
 package chat.model;
 
 import java.util.ArrayList;
+
 import twitter4j.*;
 /**
  * 
  * @author Tristan Gaebler
- * @version 0.3
+ * @version 0.4
  * 
  * This is the CTECTwitter class that will gather and sort information. It will use the Twitter API to gather data, and java to sort that data. Going to interact with twitter.
  *
@@ -15,12 +16,25 @@ public class CTECTwitter
 {
 	private ArrayList<Status> statusList;
 	private ArrayList<String> wordList;
+	private Twitter chatbotTwitter;
 	
 	public CTECTwitter()
 	{
 		statusList = new ArrayList<Status>();
 		wordList = new ArrayList<String>();
 		
+	}
+	
+	public void sendTweet(String tweet)
+	{
+		try
+		{
+			chatbotTwitter.updateStatus("I just tweeted from my Java Chatbot program! #APCSRocks @CTECNow Thanks @cscheerleader & @codyhenrichsen!");
+		}
+		catch (TwitterException e)
+		{
+			e.printStackTrace();
+		}
 	}
 	
 }
